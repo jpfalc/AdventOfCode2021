b176232cfb811abbce49ad120c5d093154b8b22f
jpfalc <31944810+jpfalc@users.noreply.github.com>
2021-12-02 10:33:35 -0500

    Removed need for -1 multiplication

--- day2.py
+++ day2.py
@@ -1,10 +1,9 @@
-from operator import pos
 import numpy as np
 
 # ---- Text Parsing Functions ----
 def values_vertical(str):
-    if str==b'up': return 1
-    elif str==b'down': return -1
+    if str==b'up': return -1
+    elif str==b'down': return 1
     else: return 0
 
 def values_horizontal(str):
@@ -18,12 +17,12 @@ data_horizontal = np.loadtxt('data/day2.txt', converters={0: values_horizontal},
 # ---- Part One ----
 motion_vertical = np.multiply(data_vertical[:, 0], data_vertical[:, 1])
 motion_horizontal = np.multiply(data_horizontal[:, 0], data_horizontal[:, 1])
-depth = -1 * sum(motion_vertical)
+depth = sum(motion_vertical)
 position = sum(motion_horizontal)
 print('Part One Answer:', depth*position)
 
 # ---- Part Two ----
-aim = np.cumsum(motion_vertical, dtype=int)
+aim = np.cumsum(motion_vertical)
 depth_chg = np.multiply(motion_horizontal, aim)
-depth = -1 * sum(depth_chg)
+depth = sum(depth_chg)
 print('Part Two Answer:', depth*position)
