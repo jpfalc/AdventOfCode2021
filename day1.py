95d63adb855baf901eed86b2653b2e1aa6441176
jpfalc <31944810+jpfalc@users.noreply.github.com>
2021-12-02 09:36:45 -0500

    Day 1

--- day1.py
+++ day1.py
@@ -1,22 +1,18 @@
 import numpy as np
 
 # ---- Load Data ----
-data = []
-with open('data/day1.txt') as file:
-    for line in file.readlines():
-        data.append(int(line))
+data = np.loadtxt('data/day1.txt', dtype=int)
 
 
 # ---- Part One ----
-data_offset1 = [0] + data[:-1]
+data_offset1 = np.append(np.array([0]), data[:-1])
 data_diff = np.subtract(data, data_offset1)
 print('Part One Answer:', np.sum(data_diff > 0) - 1)
 
 
 # ---- Part Two ----
-data_offset2 = [0] + [0] + data[:-2]
+data_offset2 =  np.append(np.array([0, 0]), data[:-2])
 sums = np.add(np.add(data, data_offset1), data_offset2)
 sums_offset1 = np.append([0], sums[:-1])
 sums_diff = np.subtract(sums, sums_offset1)
 print('Part Two Answer:', np.sum(sums_diff > 0) - 3)
-
