51b475924180b02019c7937f3b3ad26e9f581e47
jpfalc <31944810+jpfalc@users.noreply.github.com>
2021-12-02 10:29:47 -0500

    Reduced line spacing

--- day1.py
+++ day1.py
@@ -3,12 +3,10 @@ import numpy as np
 # ---- Load Data ----
 data = np.loadtxt('data/day1.txt', dtype=int)
 
-
 # ---- Part One ----
 data_offset1 = np.append(np.array([0]), data[:-1])
 data_diff = np.subtract(data, data_offset1)
 print('Part One Answer:', np.sum(data_diff > 0) - 1)
-
 
 # ---- Part Two ----
 data_offset2 =  np.append(np.array([0, 0]), data[:-2])
